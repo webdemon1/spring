1a503f1591db697fffd88ed85b79dddf531f5f87
webdemon1 <wangbaojie0318@163.com>
2019-04-04 22:18:31 +0800

    add redis module

---
+++ redis/src/main/java/com/alibaba/demon/RedisApplication.java
@@ -0,0 +1,16 @@
+package com.alibaba.demon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: Demon
+ * @create: 2019-04-04
+ **/
+@SpringBootApplication
+public class RedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApplication.class,args);
+    }
+}
---
+++ redis/src/main/java/com/alibaba/demon/config/RedisConfig.java
@@ -0,0 +1,13 @@
+package com.alibaba.demon.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: Demon
+ * @create: 2019-04-04
+ **/
+@Configuration
+public class RedisConfig {
+
+
+}
