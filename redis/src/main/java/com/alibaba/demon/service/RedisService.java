e2988f56866a9336dd0a395a93e53d7b0269c84d
webdemon1 <wangbaojie0318@163.com>
2019-04-05 18:18:10 +0800

    add test

--- redis/src/main/java/com/alibaba/demon/service/RedisService.java
+++ redis/src/main/java/com/alibaba/demon/service/RedisService.java
@@ -1,6 +1,7 @@
 package com.alibaba.demon.service;
 
 import com.alibaba.demon.domain.User;
+import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +14,9 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 /**
+ *
  * @author: Demon
  * @create: 2019-04-05
  **/
@@ -25,12 +25,9 @@ import java.util.concurrent.TimeUnit;
 public class RedisService {
 
     @Resource
-    private RedisTemplate redisTemplate;
-
-    @Resource
     private CacheManager cacheManager;
 
-    private static Map<Long,Object> DB = Maps.newConcurrentMap();
+    private static Map<Long, Object> DB = Maps.newConcurrentMap();
 
 
     private final HashOperations<String, Object, Object> hashOperation;
@@ -50,84 +47,52 @@ public class RedisService {
         return (Long) cacheManager.getCache("TEST").get("time").get();
     }
 
-    @CachePut(value = "USER",key = "#user.id")
+    @CachePut(value = "USER", key = "#user.id")
     public User saveOrUpdate(User user) {
-        DB.put(user.getId(),user);
+        DB.put(user.getId(), user);
         return user;
     }
 
-    @Cacheable(value = "USER", key = "#id",unless = "#result == null")
+    @Cacheable(value = "USER", key = "#id", unless = "#result == null")
     public User get(Long id) {
-        log.info("@RedisService.get{}",id);
+        log.info("@RedisService.get{}", id);
         return (User) DB.get(id);
     }
 
     @CacheEvict(value = "USER", key = "#id")
     public void delete(Long id) {
-        log.info("@RedisService.delete{}",id);
+        log.info("@RedisService.delete{}", id);
         DB.remove(id);
     }
 
-    public long ttl(String key) {
-        return redisTemplate.getExpire(key);
-    }
+    /**  ------ hash ----- */
 
-    public void expire(String key, long timeout) {
-        redisTemplate.expire(key, timeout, TimeUnit.SECONDS);
-    }
-
-    public long incr(String key, long delta) {
-        return valueOperations.increment(key, delta);
-    }
-
-
-    public Set<String> keys(String pattern) {
-        return redisTemplate.keys(pattern);
-    }
-
-    public void del(String key) {
-        redisTemplate.delete(key);
-    }
-
-    public void set(String key, String value) {
-        valueOperations.set(key, value);
-    }
-
-    public void set(String key, String value, long timeout) {
-        valueOperations.set(key, value, timeout, TimeUnit.SECONDS);
-    }
-
-    public Object get(String key) {
-        return valueOperations.get(key);
+    public String hGet(String key, String field) {
+        return String.valueOf( hashOperation.get(key, field).toString());
     }
 
     public void hSet(String key, String field, Object value) {
         hashOperation.put(key, field, value);
     }
 
-    public String hGet(String key, String field) {
-        return (String) hashOperation.get(key, field);
-    }
-
     public void hDel(String key, Object... fields) {
         hashOperation.delete(key, fields);
     }
 
-
     public Map<Object, Object> hGetAll(String key) {
         return hashOperation.entries(key);
     }
+    public User hashPutAndGet(User user) {
+        Map<String, Object> userMap = JSON.parseObject(JSON.toJSONString(user));
+        hashOperation.putAll(user.getId().toString(), userMap);
+        Object id = hashOperation.get(user.getId().toString(), "id");
+        Object name = hashOperation.get(user.getId().toString(), "name");
+        Object gender = hashOperation.get(user.getId().toString(), "gender");
 
-    public long lPush(String key, String value) {
-        return listOperation.leftPush(key, value);
-    }
-
-    public String lPop(String key) {
-        return (String) listOperation.leftPop(key);
-    }
-
-    public long rPush(String key, String value) {
-        return listOperation.rightPush(key, value);
+        Map<Object, Object> entries = hashOperation.entries(user.getId().toString());
+        System.out.println(entries);
+        return User.builder().name(name.toString()).id(Long.valueOf(id.toString()))
+                .gender(gender.toString()).build();
     }
 
 }
--- redis/src/test/java/com/alibaba/demon/RedisTest.java
+++ redis/src/test/java/com/alibaba/demon/RedisTest.java
@@ -5,23 +5,41 @@ import com.alibaba.demon.service.RedisService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.Assert.assertEquals;
 
 /**
+ * redis 基本数据结构
+ * <p>
+ * key-value
+ * hash
+ * set
+ * list
+ * zSet
  *
  * @author: Demon
  * @create: 2019-04-05
  **/
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = RedisApplication.class)
+@SuppressWarnings("unchecked")
 public class RedisTest {
 
     @Resource
     private RedisService redisService;
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @Test
     public void testCacheManager() {
@@ -29,7 +47,7 @@ public class RedisTest {
     }
 
     @Test
-    public void testHash() {
+    public void testCache() {
         User user = User.builder().gender("female").id(10L).name("Taylor").build();
         User u1 = redisService.saveOrUpdate(user);
         User u2 = redisService.get(user.getId());
@@ -39,4 +57,49 @@ public class RedisTest {
         User u3 = redisService.get(user.getId());
         assertEquals(null, u3);
     }
+
+    @Test
+    public void testHash() {
+        User user = User.builder().gender("male").id(1L).name("Jordon").build();
+        User u = redisService.hashPutAndGet(user);
+        assertEquals(u, user);
+        String id = redisService.hGet(user.getId().toString(), "id");
+        System.out.println(id);
+        redisService.hDel(user.getId().toString(), "id", "gender");
+        redisTemplate.delete(id);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testList() {
+        ListOperations listOperations = redisTemplate.opsForList();
+        listOperations.leftPushAll("ued", "a", "b", "c", "d");
+        List list = listOperations.range("ued", 0, -1);
+        System.out.println(list);
+        String value = (String) listOperations.rightPop("ued");
+        assertEquals("a", value);
+    }
+
+    @Test
+    public void testIncrease() {
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        valueOperations.set("order", 1, 10, TimeUnit.MINUTES);
+        valueOperations.increment("order", 1);
+        Object order = valueOperations.get("order");
+        assertEquals(2, order);
+    }
+
+    @Test
+    public void testCommon() {
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        valueOperations.set("adv", 1,1,TimeUnit.MINUTES);
+        valueOperations.set("al", 2);
+        valueOperations.set("ass", 3);
+        Set keys = redisTemplate.keys("a*");
+        assertEquals(3, keys.size());
+
+        redisTemplate.getExpire("adv");
+        redisTemplate.expire("adv", 10, TimeUnit.SECONDS);
+    }
+
 }
