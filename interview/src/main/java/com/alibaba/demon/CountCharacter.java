8ca72ce048c0b0375a1b16b025070dd1d9866950
webdemon1 <wangbaojie0318@163.com>
2019-06-05 22:45:06 +0800

    add module

---
+++ interview/src/main/java/com/alibaba/demon/CountCharacter.java
@@ -0,0 +1,14 @@
+package com.alibaba.demon;
+
+/**
+ * 给定一个全是字母的字符串
+ * 求出现最多的那个字母，如果两个字母出现次数一样，则取先出现的
+ * eg:
+ * "hello" l:2
+ * "aabb: a:2
+ */
+public class CountCharacter {
+
+
+
+}
---
+++ interview/src/main/java/com/alibaba/demon/ReadFileAndCountNum.java
@@ -0,0 +1,16 @@
+package com.alibaba.demon;
+
+/**
+ * 给一个全是字母的文件，统计每个小写字母出现的次数
+ * 并按字母的顺序输出到一个文件 “hello”
+ * eg:
+ * e:1
+ * h:1
+ * l:2
+ * o:1
+ */
+public class ReadFileAndCountNum {
+
+
+
+}
