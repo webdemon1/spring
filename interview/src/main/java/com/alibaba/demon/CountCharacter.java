3f9f1e39433b44db6a04953ba5285997147b0514
webdemon1 <wangbaojie0318@163.com>
2019-06-07 11:00:59 +0800

    done

--- interview/src/main/java/com/alibaba/demon/CountCharacter.java
+++ interview/src/main/java/com/alibaba/demon/CountCharacter.java
@@ -1,14 +1,60 @@
 package com.alibaba.demon;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * 给定一个全是字母的字符串
- * 求出现最多的那个字母，如果两个字母出现次数一样，则取先出现的
- * eg:
- * "hello" l:2
- * "aabb: a:2
+ * 求出现最多的那个字母，如果两个字母出现次数一样，则取先出现的;
  */
 public class CountCharacter {
 
 
+    public static void main(String[] args) {
+        String s1 = "ddffgrgmkjjjjhggggffg";
+        String result1 = getMaxExistsCharacter(s1);
+        System.out.println(result1);
+
+        String s2 = "ddfffrrrt";
+        String result2 = getMaxExistsCharacter(s2);
+        System.out.println(result2);
+    }
+
+    private static String getMaxExistsCharacter(String input) {
+        if (StringUtils.isBlank(input)) {
+            return null;
+        }
+
+        Map<Character, Integer> characterCountMap = new LinkedHashMap<>();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (characterCountMap.containsKey(c)) {
+                characterCountMap.put(c, characterCountMap.get(c) + 1);
+            } else {
+                characterCountMap.put(c, 1);
+            }
+        }
+
+        Set<Map.Entry<Character, Integer>> entrySet = characterCountMap.entrySet();
+        int value = 0;
+        Character c = null;
+        boolean flag = true;
+        for (Map.Entry<Character, Integer> entry : entrySet) {
+            if (flag) {
+                value = entry.getValue();
+                c = entry.getKey();
+                flag = false;
+            }
+            int characterCount = entry.getValue();
+            if (characterCount > value) {
+                value = characterCount;
+                c = entry.getKey();
+            }
+        }
+        return c + ":" + value;
+    }
 
 }
--- interview/src/main/java/com/alibaba/demon/ReadFileAndCountNum.java
+++ interview/src/main/java/com/alibaba/demon/ReadFileAndCountNum.java
@@ -1,5 +1,16 @@
 package com.alibaba.demon;
 
+import com.google.common.collect.Maps;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 /**
  * 给一个全是字母的文件，统计每个小写字母出现的次数
  * 并按字母的顺序输出到一个文件 “hello”
@@ -11,6 +22,47 @@ package com.alibaba.demon;
  */
 public class ReadFileAndCountNum {
 
+    @Test
+    public void test() throws IOException {
+        countAndWrite2File("C:\\workspace\\spring\\interview\\src\\main\\resources\\characters.txt");
+    }
 
+    private void countAndWrite2File(String path) throws IOException {
+        if (StringUtils.isBlank(path)) {
+            return;
+        }
+
+        FileReader fr = new FileReader(path);
+        int len;
+        Map<Character, Integer> characterMap = new HashMap<>();
+        while ((len = fr.read()) != -1) {
+            char c = (char) len;
+            if (!(c > 'a' && c < 'z')) {
+                continue;
+            }
+            if (characterMap.containsKey(c)) {
+                characterMap.put(c, characterMap.get(c) + 1);
+            } else {
+                characterMap.put(c, 1);
+            }
+        }
+        fr.close();
+
+        Map<Character,Integer> result = characterMap.entrySet().stream().sorted(Map.Entry.comparingByKey())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> b,
+                        Maps::newLinkedHashMap));
+
+        FileWriter fw = new FileWriter("C:\\workspace\\spring\\interview\\src\\main\\resources\\out.txt");
+        Set<Map.Entry<Character,Integer>> set = result.entrySet();
+        for (Map.Entry<Character,Integer> entry : set) {
+            Character key = entry.getKey();
+            Integer value = entry.getValue();
+            fw.write(key+":"+value);
+            fw.write("\n");
+
+        }
+       fw.close();
+    }
 
 }
+
